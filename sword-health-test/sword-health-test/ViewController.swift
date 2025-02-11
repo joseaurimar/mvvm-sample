a6f288d73d651dea4bd1669f3f8d89d466c47ea5
Jose Sepka <jose.sepka@inm.pt>
2025-02-11 10:03:21 +0000

    Created Xcode project.

---
+++ sword-health-test/sword-health-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sword-health-test
+//
+//  Created by MAC on 11/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sword-health-test/sword-health-testUITests/sword_health_testUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  sword_health_testUITestsLaunchTests.swift
+//  sword-health-testUITests
+//
+//  Created by MAC on 11/02/2025.
+//
+
+import XCTest
+
+final class sword_health_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
