a96df5ea236608e0a119c5da7b3cd9d57aedaec0
Jose Sepka <jose.sepka@inm.pt>
2025-02-13 14:51:54 +0000

    Created CarBreed database

---
+++ sword-health-test/sword-health-test/DataBase/Favourites+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Favourites+CoreDataProperties.swift
+//  sword-health-test
+//
+//  Created by MAC on 13/02/2025.
+//
+//
+
+import Foundation
+import CoreData
+
+extension Favourites {
+
+    @nonobjc public class func createFetchRequest() -> NSFetchRequest<Favourites> {
+        return NSFetchRequest<Favourites>(entityName: "Favourites")
+    }
+
+    @NSManaged public var name: String
+    @NSManaged public var url: String
+
+}
+
+extension Favourites: Identifiable {
+
+}
